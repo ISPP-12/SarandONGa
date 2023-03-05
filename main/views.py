284427a55b22cc7c44e7939ba4b4798b9c7a670b
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-03-06 00:35:24 +0100

    FIX(#36): MAKE JS FUNCTIONS WORK ON CARD DISPLAY
    I also added custom CSS to help changing the stock cards display.

--- main/views.py
+++ main/views.py
@@ -6,4 +6,4 @@ def index(request):
     return render(request, 'index.html')
 
 def stock_list(request):
-    return render(request,'stock/list.html',{"stock":[{"name":"Nuevo","slug":"nuevo","amount":12},{"name":"Nuevo","slug":"nuevo-2","amount":12},{"name":"NuevoDEMASIAADASAOADOADOAODAODO","slug":"nuevo-3","amount":1}]})
+    return render(request,'stock/list.html')
