a0b7fa201f87f8bed5d330bdd17dc20279ec0450
migromarj <miguelromeroarjona@gmail.com>
2023-03-05 20:18:23 +0100

    FIX(#016): CHANGE DB HOST AND ADD COVERAGE REQUIREMENT

--- SarandONGa/settings.py
+++ SarandONGa/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "NAME": "sarandongadb",
         "USER": "sarandonga-user",
         "PASSWORD": "sarandonga-password",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "3307",
     }
 }
