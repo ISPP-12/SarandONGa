4995f0061223eef17a4be78910d388d4250c069e
ANA CONDE MARRON <anaconmar@alum.us.es>
2023-03-05 11:32:07 +0100

    FEAT(#34): CREATION OF THE STOCK REGISTER VIEW
    Ceation that allows to create stock's objects. Fix urls.py.

--- SarandONGa/urls.py
+++ SarandONGa/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('stock-register/',views.stock_register)
+    path('', include('main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
