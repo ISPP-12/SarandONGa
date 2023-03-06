741a44abc04ce4e11ad8d7202cb45efeaa10533e
martirl11 <reyeslopezmarta2001@gmail.com>
2023-03-06 15:39:57 +0100

    FEAT(#23): CREATION OF STOCK MODEL Creation of stock app and model in order to achieve core use case management.

--- stock/models.py
+++ stock/models.py
@@ -4,6 +4,6 @@ class Stock(models.Model):
     id= models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
         return self.name
