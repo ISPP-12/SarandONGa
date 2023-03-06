1afce5732b5a296552fe22fe284841949012b215
Rafael Sanabria Espárrago <rafasana9@gmail.com>
2023-03-06 18:47:17 +0100

    FEAT(#55): CREATE FORM FOR SUBSIDIES
    Create form for subsidies. Str method fixed in Subsidy model and verbose names added in attributes so they display in Spanish.

---
+++ subsidy/forms.py
@@ -0,0 +1,14 @@
+import datetime
+from django import forms
+
+from .models import Subsidy
+
+
+class CreateNewSubsidy(forms.ModelForm):
+    class Meta:
+        model = Subsidy
+        fields = ['date', 'amount', 'name']
+        widgets = {
+            'date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': datetime.date.today}, format='%Y-%m-%d'),
+            'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': "0.01"}),
+        }
--- subsidy/views.py
+++ subsidy/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from subsidy.models import Subsidy
+from .forms import *
+
 # Create your views here.
+
+
+def subsidy(request):
+    if request.method == "POST":
+        form = CreateNewSubsidy(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = CreateNewSubsidy()
+    return render(request, 'subsidy_form.html', {"form": form})
