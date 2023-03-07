dc301cdae72eb8d5e7d0be11e94926562c2b1c7b
JaimeBorregoConde <jaimeborregoconde@gmail.com>
2023-03-07 18:49:45 +0100

    FIX(#125): FIX SOME CODACY PROBLEMS There was a unused line that create 1 issues. It has been commented, due t the fact that we may need it soon.

--- person/models.py
+++ person/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.core.validators import MinValueValidator
-from django.core.validators import RegexValidator
+#from django.core.validators import RegexValidator
 
 SEX_TYPES = (
     ('F','Femenino'),
