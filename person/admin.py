1780aa00a84d1a5a55203d0711a7260c3260900a
Tomas Camero <tcamerob@gmail.com>
2023-03-07 12:30:46 +0100

    FEAT(#104): CREATE GODFATHER MODEL
    I have additionally modified the Person model and the admin.py file so that there are no errors.

--- person/admin.py
+++ person/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import Person
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name','surname','email', 'sex', 'resident_place', 'address', 'telephone', 'postal_code')
-    list_filter = ('id', 'name','surname','email', 'sex')
-
-    search_fields = ('id', 'name','surname','email', 'sex', 'resident_place', 'telephone', 'postal_code')
-
-admin.site.register(Person)
+admin.site.register(GodFather)
--- person/models.py
+++ person/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.core.validators import MinValueValidator
+from django.core.validators import RegexValidator
 
 SEX_TYPES = (
     ('F','Femenino'),
@@ -8,7 +10,24 @@ SEX_TYPES = (
     ('O', 'Otro'),
 )
 
-class Person(AbstractUser):
+PAYMENT_METHOD = (
+    ('T', 'Transferencia'),
+    ('TB','Tarjeta Bancaria')
+)
+
+STATUS = (
+    ('C', 'Casado/a'),
+    ('F', 'Fallecido/a'),
+    ('V', 'Viudo/a'),
+    ('S', 'Soltero/a')  
+)
+
+FREQUENCY = (
+    ('A', 'Anual'),
+    ('M', 'Mensual')
+)
+
+class Person(models.Model):
 
     id= models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, verbose_name="Nombre")
@@ -24,10 +43,23 @@ class Person(AbstractUser):
     is_worker = models.BooleanField(default=False, verbose_name="¿Es trabajador?") #Extiende a clase WorkerProfile si es un trabajador
 
     class Meta:
-        db_table = 'person'
+        abstract = True
 
-class WorkerProfile(models.Model):
-    Person = models.OneToOneField(Person, on_delete=models.CASCADE)
+class GodFather(Person):
+    dni = models.CharField(max_length=9, unique=True,verbose_name='DNI')
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD,verbose_name='Método de pago',)
+    bank_account_number = models.CharField(max_length=20,verbose_name='Número de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
+    bank_account_holder = models.CharField(max_length=100,verbose_name='Titular de cuenta bancaria')
+    bank_account_reference = models.CharField(max_length=100,verbose_name='Referencia de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
+    amount = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Cantidad',validators=[MinValueValidator(1)])
+    frequency = models.CharField(max_length=20, choices=FREQUENCY,verbose_name='Frecuencia de pago')
+    seniority = models.CharField(max_length=50,verbose_name='Antiguedad')
+    notes = models.TextField(blank=True,verbose_name='Observaciones')
+    status = models.CharField(max_length=20, choices=STATUS,verbose_name='Estado')
+    #T0D0
+    #Añadir relacion uno a muchos con entidad pago
+
+#class WorkerProfile(models.Model):
+    #Person = models.OneToOneField(Person, on_delete=models.CASCADE)
     #ONG = models.OneToOneField(ONG, on_delete=models.CASCADE) #ONG en la que trabaja
     #active = models.BooleanField(default=True) #¿Sigue trabajando en la ONG?
-
