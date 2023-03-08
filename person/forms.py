6f4e6e855481256ff105ca5373819c862df27135
JaimeBorregoConde <jaimeborregoconde@gmail.com>
2023-03-08 20:58:41 +0100

    FIX(#125): FIX SOME FORM PROBLEMS Change in dofather creation form due to an unnecessary parameter in it (is_worker)

--- person/forms.py
+++ person/forms.py
@@ -6,7 +6,7 @@ from .models import GodFather
 class CreateNewGodFather(forms.ModelForm):
     class Meta:
         model = GodFather
-        fields = ['dni','name','surnames','email','birth_date','sex','city','address','telephone','postal_code','is_worker', 'payment_method', 'bank_account_number','bank_account_holder','bank_account_reference','amount','frequency','seniority','notes','status']
+        fields = ['dni','name','surnames','email','birth_date','sex','city','address','telephone','postal_code', 'payment_method', 'bank_account_number','bank_account_holder','bank_account_reference','amount','frequency','seniority','notes','status']
         widgets = {
             'birth_date': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
             'seniority': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
