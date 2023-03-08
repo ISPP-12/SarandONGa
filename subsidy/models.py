ca0f3eac20cffcb331f360328fb4e3bef8570713
juradosalado <juansaladous@gmail.com>
2023-03-08 18:23:19 +0100

    TEST(#69): TEST SUBSIDY CREATION
    Test subsidy creation with positive and negative cases

--- subsidy/models.py
+++ subsidy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
@@ -10,7 +11,7 @@ class Subsidy(models.Model):
     date = models.DateField(verbose_name="Fecha")
 
     # Importe de la subvención
-    amount = models.FloatField(verbose_name="Cantidad")
+    amount = models.FloatField(verbose_name="Cantidad", validators=[MinValueValidator(0)])
 
     # Nombre de la persona o entidad que dona
     name = models.CharField(max_length=100, verbose_name="Nombre")
--- subsidy/tests.py
+++ subsidy/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
 
+from subsidy.models import Subsidy
+
 # Create your tests here.
+
+class SubsidyTestCase(TestCase):
+    def setUp(self):
+        Subsidy.objects.create(date="2021-01-01", amount=1000, name="Juan")
+        Subsidy.objects.create(date="2021-01-02", amount=2000, name="Pedro")
+        Subsidy.objects.create(date="2021-01-03", amount=3000, name="Maria")
+    
+    def test_subsidy_create(self):
+        subsidy = Subsidy.objects.get(name="Juan")
+        self.assertEqual(subsidy.name, "Juan")
+        self.assertEqual(subsidy.amount, 1000)
+        self.assertEqual(subsidy.date, "2021-01-01")
+    
+    def test_subsidy_delete(self):
+        subsidy = Subsidy.objects.get(name="Juan")
+        subsidy.delete()
+        self.assertEqual(Subsidy.objects.count(), 2)
+
+    def test_subsidy_update(self):
+        subsidy = Subsidy.objects.get(name="Juan")
+        subsidy.name = "Juanito"
+        subsidy.amount = 17
+        subsidy.date = "2017-07-17"
+        subsidy.save()
+        self.assertEqual(subsidy.name, "Juanito")
+        self.assertEqual(subsidy.amount, 17)
+        self.assertEqual(subsidy.date, "2017-07-17")
+
+    #Create test
+    def test_subsidy_create_date_incorrect(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date="This is a date incorrect", amount=1000, name="Juan")
+
+    def test_subsidy_create_amount_negative(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date="2021-01-01", amount=-1000, name="Juan")
+    
+    def test_subsidy_create_name_max_length_incorrect(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date="2021-01-01", amount=1000, name="Juan"*100)
+    
+    def test_subsidy_create_name_blank(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date="2021-01-01", amount=1000, name="")
+    
+    def test_subsidy_create_amount_null(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date="2021-01-01", amount=None, name="Juan")
+    
+    def test_subsidy_create_date_null(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(date=None, amount=1000, name="Juan")
+            
