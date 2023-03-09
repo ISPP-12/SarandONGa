1d8236ad519a33012a66194d1e2bd9520f2f6075
migromarj <miguelromeroarjona@gmail.com>
2023-03-09 19:20:38 +0100

    REFACTOR(#105): PUT ALL OPTION TUPLES USED FOR MODELS ONE AFTER ANOTHER
    Changes made after suggestion, in the pull request, by the reviewer.

--- person/models.py
+++ person/models.py
@@ -30,39 +30,6 @@ FREQUENCY = (
     ('S','Semestral'),
 )
 
-class Person(models.Model):
-
-    id= models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, verbose_name="Nombre")
-    surnames = models.CharField(max_length=200, verbose_name="Apellidos")
-    email = models.CharField(max_length=200, verbose_name="Correo electrónico")
-    birth_date = models.DateTimeField(default=timezone.now, verbose_name="Fecha de nacimiento")
-    sex = models.CharField(max_length=50, choices=SEX_TYPES, verbose_name="Género")
-    city = models.CharField(max_length=200, verbose_name="Ciudad")
-    address = models.CharField(max_length=200, verbose_name="Dirección")
-    telephone = models.IntegerField(verbose_name="Teléfono")
-    postal_code = models.IntegerField(verbose_name="Código postal")
-
-    is_worker = models.BooleanField(default=False, verbose_name="¿Es trabajador?") #Extiende a clase WorkerProfile si es un trabajador
-
-    class Meta:
-        abstract = True
-
-class GodFather(Person):
-    dni = models.CharField(max_length=9, unique=True,verbose_name='DNI')
-    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD,verbose_name='Método de pago',)
-    bank_account_number = models.CharField(max_length=24,verbose_name='Número de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
-    bank_account_holder = models.CharField(max_length=100,verbose_name='Titular de cuenta bancaria')
-    bank_account_reference = models.CharField(max_length=100,verbose_name='Referencia de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
-    amount = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Cantidad',validators=[MinValueValidator(1)])
-    frequency = models.CharField(max_length=20, choices=FREQUENCY,verbose_name='Frecuencia de pago')
-    seniority = models.DateField(verbose_name='Antigüedad')
-    notes = models.TextField(blank=True,verbose_name='Observaciones')
-    status = models.CharField(max_length=20, choices=STATUS,verbose_name='Estado')
-    #T0D0
-    #Añadir relacion uno a muchos con entidad pago
-
-
 CONDITION = (
     ('EM', 'Escleorosis múltiple'),
     ('ICTUS', 'Ictus'),
@@ -96,6 +63,38 @@ HOUSING_TYPE = (
     ('VC', 'Vivienda compartida'),
     ('VP', 'Vivienda propia')
 )
+
+class Person(models.Model):
+
+    id= models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, verbose_name="Nombre")
+    surnames = models.CharField(max_length=200, verbose_name="Apellidos")
+    email = models.CharField(max_length=200, verbose_name="Correo electrónico")
+    birth_date = models.DateTimeField(default=timezone.now, verbose_name="Fecha de nacimiento")
+    sex = models.CharField(max_length=50, choices=SEX_TYPES, verbose_name="Género")
+    city = models.CharField(max_length=200, verbose_name="Ciudad")
+    address = models.CharField(max_length=200, verbose_name="Dirección")
+    telephone = models.IntegerField(verbose_name="Teléfono")
+    postal_code = models.IntegerField(verbose_name="Código postal")
+
+    is_worker = models.BooleanField(default=False, verbose_name="¿Es trabajador?") #Extiende a clase WorkerProfile si es un trabajador
+
+    class Meta:
+        abstract = True
+
+class GodFather(Person):
+    dni = models.CharField(max_length=9, unique=True,verbose_name='DNI')
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD,verbose_name='Método de pago',)
+    bank_account_number = models.CharField(max_length=24,verbose_name='Número de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
+    bank_account_holder = models.CharField(max_length=100,verbose_name='Titular de cuenta bancaria')
+    bank_account_reference = models.CharField(max_length=100,verbose_name='Referencia de cuenta bancaria', validators=[RegexValidator(r'^[0-9]+$')])
+    amount = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Cantidad',validators=[MinValueValidator(1)])
+    frequency = models.CharField(max_length=20, choices=FREQUENCY,verbose_name='Frecuencia de pago')
+    seniority = models.DateField(verbose_name='Antigüedad')
+    notes = models.TextField(blank=True,verbose_name='Observaciones')
+    status = models.CharField(max_length=20, choices=STATUS,verbose_name='Estado')
+    #T0D0
+    #Añadir relacion uno a muchos con entidad pago
 
 class ASEMUser(Person):
     condition = models.CharField(max_length=20, choices=CONDITION,verbose_name='Condición médica')
