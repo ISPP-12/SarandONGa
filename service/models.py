3e635e5825b704932fb4d49362a34221f6f05697
juradosalado <juansaladous@gmail.com>
2023-03-09 19:41:51 +0100

    REFACTOR(#116): ADD MINOR CHANGES FOR CODACY CHECK FOR THIRD TIME
    I tried to solve codacy problems again

--- service/models.py
+++ service/models.py
@@ -22,7 +22,6 @@ class ServiceAmount(models.Model):
         user_type = models.CharField(max_length=50, choices=person_models.ASEMUSER_TYPE,verbose_name="Tipo de usuario ASEM")
         amount = models.FloatField(verbose_name="Precio")
         date = models.DateTimeField(default=timezone.now)
-
         def __str__(self):
             return self.service_type
 
@@ -34,13 +33,11 @@ class Service(models.Model):
     #TODO: añadir una foreign key con pago cuando pago esté implementado
     #payment = models.ForeignKey(Payment, verbose_name="Pago")
     asem_user = models.ForeignKey(person_models.ASEMUser, verbose_name="Usuario ASEM", on_delete=models.CASCADE)
-
     @property
     def amount(self):
         try:
             return ServiceAmount.objects.filter(service_type=self.service_type, user_type=self.asem_user.user_type, date__lte=self.date).latest('date').amount
         except ServiceAmount.DoesNotExist:
             return 0
-
     def __str__(self):
         return self.service_type
