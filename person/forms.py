81894ac825b2b81cd8729aa3d78b7799b45f5b52
JaimeBorregoConde <jaimeborregoconde@gmail.com>
2023-03-09 21:06:16 +0100

    FIX(#125): FIX CODACY PROBLEMS >Last quick fix create another issue, so the fields in form has been change to comply with Codacy standars

--- person/forms.py
+++ person/forms.py
@@ -7,9 +7,9 @@ class CreateNewGodFather(forms.ModelForm):
     class Meta:
         model = GodFather
         fields = ['dni','name','surnames','email','birth_date','sex','city',
-                 'address','telephone','postal_code', 'payment_method', 
-                 'bank_account_number','bank_account_holder','bank_account_reference',
-                 'amount','frequency','seniority','notes','status']
+                  'address','telephone','postal_code', 'payment_method',
+                  'bank_account_number','bank_account_holder','bank_account_reference',
+                  'amount','frequency','seniority','notes','status']
         widgets = {
             'birth_date': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
             'seniority': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
