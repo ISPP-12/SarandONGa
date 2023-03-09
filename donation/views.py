181583e221c2ef17e0e088a6c33e67ceb79ada80
IsmaelP19 <ismperort@alum.us.es>
2023-03-09 14:57:45 +0100

    FIX(#48): MODIFY DONATIONS ATTRIBUTE

--- donation/views.py
+++ donation/views.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
-            return obj.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            return obj.strftime('%d/%m/%Y')
         elif isinstance(obj, Decimal):
             return float(obj)
         return super().default(obj)
@@ -17,18 +17,17 @@ class CustomJSONEncoder(json.JSONEncoder):
 def donations_list(request):
     # get donations from database
     donations = Donation.objects.all()
-    
-
-    for donation in donations:
-        created_date = donation.created_date
-        modified_date = created_date.strftime('%d/%m/%Y')
-        donation.created_date = modified_date
 
     donations_dict = [obj.__dict__ for obj in donations]
     for d in donations_dict:
         d.pop('_state', None)
 
     donations_json = json.dumps(donations_dict, cls=CustomJSONEncoder)
+
+    for donation in donations:
+        created_date = donation.created_date
+        modified_date = created_date.strftime('%d/%m/%Y')
+        donation.created_date = modified_date
 
 
     return render(request, 'donation/list.html', {'objects': donations, 'objects_json': donations_json, 'object_name': 'donación', 'title': 'Gestión de donaciones'})
