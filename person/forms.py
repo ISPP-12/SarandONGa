982137d4678b1374861f66205016ae4f2f1eedff
Jaime Moscoso Bernal <jaimesfc@live.com>
2023-03-10 14:57:08 +0100

    FEAT( #28 ): ADD WORKER CREATION FORM
    Added function and form for creating a new worker

--- person/forms.py
+++ person/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
-from .models import ASEMUser
+from .models import ASEMUser, Worker
 
 
 class CreateNewASEMUser(forms.ModelForm):
     class Meta:
         model = ASEMUser
         exclude = ['id']
+
+class CreateNewWorker(forms.ModelForm):
+    class Meta:
+        model = Worker
+        #Campos de admin y activo están excluidos en la creación porque
+        #todos los trabajadores nuevos deben tenerlo marcado
+        exclude = ['id', 'last_login', 'is_active','is_admin']
+
