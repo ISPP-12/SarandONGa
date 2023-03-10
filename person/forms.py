62726055bd85472d398b2cfc1b4fd37b2af7c082
Jaime Moscoso Bernal <jaimesfc@live.com>
2023-03-10 21:30:34 +0100

    FIX(#28): HIDE PASSWORD INPUT
    Hided the password input on the view

--- person/forms.py
+++ person/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-
 from .models import ASEMUser, Worker
+from django.contrib.auth.decorators import login_required
 
 
 class CreateNewASEMUser(forms.ModelForm):
@@ -9,9 +9,29 @@ class CreateNewASEMUser(forms.ModelForm):
         exclude = ['id']
 
 class CreateNewWorker(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmar contraseña', widget=forms.PasswordInput)
+
     class Meta:
         model = Worker
-        #Campos de admin y activo están excluidos en la creación porque
-        #todos los trabajadores nuevos deben tenerlo marcado
-        exclude = ['id', 'last_login', 'is_active','is_admin']
+        exclude = ['id', 'last_login', 'is_active','is_admin','password']
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(CreateNewWorker, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
 
