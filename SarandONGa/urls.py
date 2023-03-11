f7ab2583f868df8ce789c789bf0129cc4c040647
Tomas Camero <tcamerob@gmail.com>
2023-03-11 14:18:29 +0100

    FIX(#115): FIX PROBLEM

--- SarandONGa/urls.py
+++ SarandONGa/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('donations/', include(donation_urls), name='donations'),
     path('subsidy/list', subsidy_views.subsidy_list, name="subsidy"),
     path('service/', include(service_urls),name="service"),
-    path('worker/create', person_views.create_worker, name="worker")
+    path('worker/create', person_views.create_worker, name="worker"),
     path('service/list', service_views.service_list, name="service_list"),
 
 ]
