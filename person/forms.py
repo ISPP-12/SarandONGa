7f8dae287307ab730b2d2c6322ef0543da07efe2
JaimeBorregoConde <jaimeborregoconde@gmail.com>
2023-03-11 18:53:18 +0100

    FIX(#125): FIX SOME CODACY AND GITHUB ACTIONS PROBLEM Due to new implementations an changes in cogfather models, the form was deprecated. Furthermore, 3 changes has been made in order to reach Codacy standars.

--- person/forms.py
+++ person/forms.py
@@ -1,17 +1,11 @@
-
 import datetime
 from django import forms
-
-from .models import GodFather
-
-from django import forms
-
-from .models import ASEMUser
+from .models import GodFather, ASEMUser
 
 class CreateNewGodFather(forms.ModelForm):
     class Meta:
         model = GodFather
-        fields = ['dni','name','surnames','email','birth_date','sex','city',
+        fields = ['dni','name','surname','email','birth_date','sex','city',
                   'address','telephone','postal_code', 'payment_method',
                   'bank_account_number','bank_account_holder','bank_account_reference',
                   'amount','frequency','seniority','notes','status']
@@ -20,7 +14,7 @@ class CreateNewGodFather(forms.ModelForm):
             'seniority': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
             'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': "0.01"}),
         }
-        
+
 class CreateNewASEMUser(forms.ModelForm):
     class Meta:
         model = ASEMUser
