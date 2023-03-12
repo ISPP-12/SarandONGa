0c428b0dabbd16b62d8b88b57a17a60bf67de373
Alejandro Sanchez <80268869+alexsm01@users.noreply.github.com>
2023-03-12 01:30:36 +0100

    FIX(#24): URL BUG FIXED
    A little error after solving conflicts has been fixed

--- person/urls.py
+++ person/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('worker/create', views.create_worker, name="worker"),
     path('asem_user/', views.asem_user, name="asem_user"),
     path('asem/asem_user_list', views.asem_user_list, name="asem_user_list"),
-    path('godfather/list', views.godfather_list, name="godfather_list"),y
+    path('godfather/list', views.godfather_list, name="godfather_list"),
     path('godfather/create', views.godfather_create, name="subsidy"),
     path('child/create', views.create_child, name="new_child"),
 ]
