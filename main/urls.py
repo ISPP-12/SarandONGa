421ce6e42da36c42418be6bdd9882a82b353101c
ezegonmac <ezegonmac01@gmail.com>
2023-03-13 18:25:37 +0100

    FIX(#86): FIX CODACY ISSUES

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 import donation.urls as donation_urls
-import subsidy.urls as subsidies_urls
+
 
 urlpatterns = [
     path('', views.index, name='home'),
