a7074a20d3064965c62639e64c764e9131d15563
IsmaelP19 <ismperort@alum.us.es>
2023-03-14 17:45:03 +0100

    FIX(#146): FIX SOME BUTTONS

--- service/forms.py
+++ service/forms.py
@@ -6,3 +6,8 @@ class CreateNewService(forms.ModelForm):
     class Meta:
         model = Service
         exclude = ['id']
+
+    # to remove the first empty choice '---------'
+    def __init__(self, *args, **kwargs):
+        super(CreateNewService, self).__init__(*args, **kwargs)
+        self.fields['service_type'].choices = self.fields['service_type'].choices[1:]
