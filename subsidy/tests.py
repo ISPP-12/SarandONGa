7fd7069cffc8e92f4a6ee3f7153387ed6a051754
juradosalado <juansaladous@gmail.com>
2023-03-16 17:40:50 +0100

    FIX(#69): SOLVE PROBLEMS CAUSED BY MODEL UPDATE
    Now the tests run with no failures

--- subsidy/tests.py
+++ subsidy/tests.py
@@ -6,15 +6,19 @@ from subsidy.models import Subsidy
 
 class SubsidyTestCase(TestCase):
     def setUp(self):
-        Subsidy.objects.create(date="2021-01-01", amount=1000, name="Juan")
-        Subsidy.objects.create(date="2021-01-02", amount=2000, name="Pedro")
-        Subsidy.objects.create(date="2021-01-03", amount=3000, name="Maria")
+        Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism= "ONG1",provisional_resolution=True, final_resolution=False, amount=1000, name="Juan")
+        Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism="ONG2",provisional_resolution=True, final_resolution=False, amount=1000, name="Pedro")
+        Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism="ONG3",provisional_resolution=True, final_resolution=False, amount=1000, name="Maria")
 
     def test_subsidy_create(self):
         subsidy = Subsidy.objects.get(name="Juan")
         self.assertEqual(subsidy.name, "Juan")
         self.assertEqual(subsidy.amount, 1000)
-        self.assertEqual(str(subsidy.date), "2021-01-01")
+        self.assertEqual(str(subsidy.presentation_date), "2021-01-01")
+        self.assertEqual(str(subsidy.payment_date), "2021-01-02")
+        self.assertEqual(subsidy.provisional_resolution, True)
+        self.assertEqual(subsidy.final_resolution, False)
+        self.assertEqual(subsidy.organism, "ONG1")
 
     def test_subsidy_delete(self):
         subsidy = Subsidy.objects.get(name="Juan")
@@ -25,33 +29,49 @@ class SubsidyTestCase(TestCase):
         subsidy = Subsidy.objects.get(name="Juan")
         subsidy.name = "Juanito"
         subsidy.amount = 17
-        subsidy.date = "2017-07-17"
+        subsidy.presentation_date = "2017-07-17"
+        subsidy.payment_date = "2017-07-18"
+        subsidy.provisional_resolution = False
+        subsidy.final_resolution = True
+        subsidy.organism = "ONG4"
         subsidy.save()
         self.assertEqual(subsidy.name, "Juanito")
         self.assertEqual(subsidy.amount, 17)
-        self.assertEqual(subsidy.date, "2017-07-17")
+        self.assertEqual(str(subsidy.presentation_date), "2017-07-17")
+        self.assertEqual(str(subsidy.payment_date), "2017-07-18")
+        self.assertEqual(subsidy.provisional_resolution, False)
+        self.assertEqual(subsidy.final_resolution, True)
+        self.assertEqual(subsidy.organism, "ONG4")
 
     #Create test
-    def test_subsidy_create_date_incorrect(self):
+    def test_subsidy_create_presentation_date_incorrect(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date="This is a date incorrect", amount=1000, name="Juan")
+            Subsidy.objects.create(presentation_date="This is a date incorrect", payment_date="2021-01-02", organism="ONG2",provisional_resolution=True, final_resolution=False, amount=1000, name="Pedro")
+
+    def test_subsidy_create_payment_date_incorrect(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="This is a date incorrect", organism="ONG2",provisional_resolution=True, final_resolution=False, amount=1000, name="Pedro")
+
+    def test_create_subsidy_organism_max_length_incorrect(self):
+        with self.assertRaises(Exception):
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism="ONG2"*100,provisional_resolution=True, final_resolution=False, amount=1000, name="Pedro")
 
     def test_subsidy_create_amount_negative(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date="2021-01-01", amount=-10.8, name="Juan")
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism="ONG2",provisional_resolution=True, final_resolution=False, amount=-1000, name="Pedro")
 
     def test_subsidy_create_name_max_length_incorrect(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date="2021-01-01", amount=1000, name="Juan"*100)
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism= "ONG1",provisional_resolution=True, final_resolution=False, amount=1000, name="Juan"*100)
 
     def test_subsidy_create_name_null(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date="2021-01-01", amount=1000, name=None)
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism= "ONG1",provisional_resolution=True, final_resolution=False, amount=1000, name=None)
 
     def test_subsidy_create_amount_null(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date="2021-01-01", amount=None, name="Juan")
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism= "ONG1",provisional_resolution=True, final_resolution=False, amount=None, name="Juan")
 
-    def test_subsidy_create_date_null(self):
+    def test_subsidy_create_organism_null(self):
         with self.assertRaises(Exception):
-            Subsidy.objects.create(date=None, amount=1000, name="Juan")
+            Subsidy.objects.create(presentation_date="2021-01-01", payment_date="2021-01-02", organism=None,provisional_resolution=True, final_resolution=False, amount=1000, name="Juan")
