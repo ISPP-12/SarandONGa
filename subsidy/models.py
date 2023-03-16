627a2a2f32d147e7b26fce7bf25f7c894e0ef8fe
Juan Salado Jurado <80246665+juradosalado@users.noreply.github.com>
2023-03-16 17:42:24 +0100

    Revert "TEST(#69): TEST SUBSIDY CREATION"

--- subsidy/models.py
+++ subsidy/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.core.validators import MinValueValidator
-from django.forms import ValidationError
+
+# Create your models here.
+
 
 class Subsidy(models.Model):
 
@@ -15,17 +16,10 @@ class Subsidy(models.Model):
     provisional_resolution = models.BooleanField(verbose_name="Resolución provisional", default=False)
     final_resolution = models.BooleanField(verbose_name="Resolución definitiva", null=True)
     # Importe de la subvención
-    amount = models.FloatField(validators=[MinValueValidator(0)], verbose_name="Cantidad")
+    amount = models.FloatField(verbose_name="Cantidad")
 
     # Nombre completo (con apellidos) de la persona o entidad que dona
     name = models.CharField(max_length=200, verbose_name="Nombre completo")
-
-    def save(self, force_insert=False, force_update=False, using=None,
-          update_fields=None):
-        self.clean()
-        if self.amount < 0:
-            raise ValidationError("El importe no puede ser negativo")
-        super().save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
         return self.name
