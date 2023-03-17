e02f122ea05c5b8b53386e5956e6904184ebd98a
IsmaelP19 <ismperort@alum.us.es>
2023-03-18 00:38:46 +0100

    FEATURE(#236): MODIFY INPUT COMPONENT

--- payment/forms.py
+++ payment/forms.py
@@ -9,7 +9,7 @@ class create_payment_form(forms.ModelForm):
         fields = ['amount', 'payday']
         widgets = {
             'payday': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.date.today}, format='%Y-%m-%d'),
-            'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': "0.01"}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': "0.01", "placeholder": "Escriba una cantidad"}),
             # Cuando esté padrino, descomentar la línea de abajo
             #'godfather': forms.Select(attrs={'class': 'form-control'}),
         }
