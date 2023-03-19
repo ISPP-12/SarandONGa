01befd0429762fd06252302012b8972cc1d2a311
Rafael Sanabria Espárrago <rafasana9@gmail.com>
2023-03-19 21:34:02 +0100

    FIX(#297): ADDED STR METHOD TO ONG MODEL
    Added method str to ONG model so the name shows in select inputs.

--- ong/models.py
+++ ong/models.py
@@ -5,3 +5,6 @@ class Ong(models.Model):
     name = models.CharField(max_length=255, verbose_name="Nombre")
     # FALTA RELACION A PROYECTOS CUANDO SE CREE
     # RELACION A DONACION, SUBVENCION, STOCK, PERSONA HECHAS, SOLO REVISAR
+
+    def __str__(self):
+        return self.name
