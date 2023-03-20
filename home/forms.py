2bf6fb0191915651db8ea5ee8b07aba7dd8b4ad2
martirl11 <reyeslopezmarta2001@gmail.com>
2023-03-20 19:33:46 +0100

    FIX(#270): SPONSORSHIP MODEL, DELETE AND LIST Correct date of the forms.

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Home
-from datetime import datetime
+from datetime import date
 
 class create_home_form(forms.ModelForm):
     class Meta:
@@ -9,6 +9,6 @@ class create_home_form(forms.ModelForm):
                   'bank_account_number','bank_account_holder','bank_account_reference',
                   'amount','frequency','seniority','notes']
         widgets = {
-            'seniority': forms.DateInput(attrs={'class': 'form-control', 'value': datetime.today()}, format='%Y-%m-%d'),
-            'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': "0.01"}),
+            'seniority': forms.DateInput(attrs={'class': 'form-control','type': 'date', 'value': date.today()}, format='%Y-%m-%d'),
+            'amount': forms.NumberInput(attrs={'class': 'form-control','type': 'date', 'step': "0.01"}),
         }
