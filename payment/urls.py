812e4f3630adee5443a6dc73661bb6d3179ded85
alexsm01 <alejsm01@gmail.com>
2023-03-22 00:50:47 +0100

    FIX(#276): BUGS SOLVED
    Template and url errors have been solved

--- payment/urls.py
+++ payment/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('create', views.payment_create, name="payment_create"),
     path('<int:payment_id>/delete', views.payment_delete, name="payment_delete"),
     path('<int:payment_id>/update', views.payment_update, name="payment_update"),
-    path('<int:pk>', views.payment_details, name="payment_details"),
+    path('<int:payment_id>', views.payment_details, name="payment_details"),
 
 
 ]
