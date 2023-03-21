b804ab2b68c74c2d1aeda94319ff03c10bc74fd4
ezegonmac <ezegonmac01@gmail.com>
2023-03-21 17:55:52 +0100

    FEAT(#254): ADD BASE CALENDAR COMPONENT

--- main/views.py
+++ main/views.py
@@ -7,12 +7,12 @@ def index(request):
 
 
 def components(request):
-    
+
     context = {
         'options1': {'1':'1','2':'2','3':'3','4':'4'},
         'options2': {'4':'5','9':'8'},
         'stockTest': {'name':'STOCKINGS','quantity':9999},
         'object_name': 'ejemplo',
     }
-    
+
     return render(request, 'components.html', context)
