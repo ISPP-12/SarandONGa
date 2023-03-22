178553d50c64e896a48b1cfae1d66a4b03d34585
Jaime Moscoso Bernal <jaimesfc@live.com>
2023-03-22 21:24:47 +0100

    FEAT(#213): CREATE STOCK UPDATE FUNCTION
    Created a stock update function. As we currently have no login function in the application, it is not possible to automatically assign the ONG to the stock. Therefore, I have not taken the ONG into account.

--- stock/views.py
+++ stock/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Stock
+from django.contrib import messages
 from .forms import CreateNewStock
 
 def stock_list(request):
@@ -22,5 +23,20 @@ def stock_create(request):
             d = Stock(name = name, quantity = quantity)
             d.save()
             return redirect('stock_list')
+        else:
+            messages.error(request, 'Formulario con errores')
     form = CreateNewStock()
     return render(request, 'stock/register.html', {'form': form, 'title': 'Registrar artículo'})
+
+def stock_update(request, stock_id):
+    stock = get_object_or_404(Stock, id=stock_id)
+    if request.method == "POST":
+        form = CreateNewStock(request.POST, instance=stock)
+        if form.is_valid():
+            form.save()
+            return redirect('stock_list')
+        else:
+            messages.error(request, 'Formulario con errores')
+
+    form = CreateNewStock(instance=stock)
+    return render(request, 'stock/register.html', {'form': form, 'title': 'Actualizar artículo'})
