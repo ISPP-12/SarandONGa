b606f034c75a1fb36e744b626a3379f84f7786cd
Rafael Sanabria Espárrago <rafasana9@gmail.com>
2023-03-23 20:02:12 +0100

    FIX(#229): FIX PAYMENT MODEL
    Fixed project reference in payment model.

--- payment/models.py
+++ payment/models.py
@@ -11,7 +11,7 @@ class Payment(models.Model):
         default=timezone.now, verbose_name="Día de cobro")
     amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[
                                  MinValueValidator(0)], verbose_name="Importe")
-    proyect = models.ForeignKey(
+    project = models.ForeignKey(
         Project, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Proyecto")
 
     # ACTUALMENTE ESTO FALLA PORQUE SERVICIO Y PADRINO NO EXISTEN
