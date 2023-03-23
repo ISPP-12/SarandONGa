e7e68f0e501d5b67778ce2028bae58e7281b080c
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-03-23 23:48:28 +0100

    FEAT(#329): MAKE PROVISIONAL PROYECT VIEW
    Until all backend is in develop

--- proyect/urls.py
+++ proyect/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create',views.proyect_create, name='proyect_create'),
+    path('list',views.proyect_list, name='proyect_list'),
     path('<int:proyect_id>/delete',
         views.proyect_delete, name="proyect_delete"),
 ]
