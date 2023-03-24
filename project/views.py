a71ee38f039a9808401fce9e85dd89d6f261065c
migromarj <miguelromeroarjona@gmail.com>
2023-03-24 18:16:29 +0100

    FEAT(#221): ADD PROJECT DETAILS FUNCTION
    A test view has been carried out to verify correct operation.

--- project/views.py
+++ project/views.py
@@ -40,3 +40,7 @@ def project_update(request, project_id):
 
     form = CreateNewProject(instance=project)
     return render(request, 'project/project_form.html', {'form': form, 'title': 'Actualizar proyecto'})
+
+def project_details(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, 'project/project_details.html', {'project': project})
