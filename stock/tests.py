4c6b7c42e869ae5bf04c054ce15617ae082d6730
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-03-26 22:01:32 +0200

    FIX(#335): PREPARE THE PROJECT FOR SELENIUM
    Chrome drivers aren't working for me

--- stock/tests.py
+++ stock/tests.py
@@ -4,6 +4,18 @@ from django.test import TestCase
 from ong.models import Ong
 from .models import Stock
 from decimal import Decimal
+from person.models import Worker
+
+from datetime import datetime
+
+# SELENIUM IMPORTS
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
 
 class StockTestCase(TestCase):
     def setUp(self):
@@ -154,4 +166,48 @@ class StockTestCase(TestCase):
             stock.quantity = -1
             stock.full_clean()
 
+class StockListViewTestCaseVidessur(StaticLiveServerTestCase):
+    def setUp(self):
+        super().setUp()
+        
+        self.ong = Ong(name='VidesSur')
+        self.test_stock_1 = Stock(name="Test stock 1", ong=self.ong, quantity=150)
 
+        self.ong.save()
+        self.test_stock_1.save()
+
+        self.usersuper = Worker(
+            email="test@email.com",
+            name="Test Person",
+            surname="Test Apellido",
+            birth_date=datetime(2001,3,14),
+            sex='M',
+            city='Test Ciudad',
+            address='Test Calle',
+            telephone='123456789',
+            postal_code='41010',
+        )
+        self.usersuper.ong = self.ong
+        self.usersuper.set_password('adminTest')
+        self.usersuper.is_admin = True
+        self.usersuper.save()
+
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+
+        self.driver.get(f'{self.live_server_url}/login/')
+        self.driver.find_element(By.ID,"id_username").send_keys('test@email.com')
+        self.driver.find_element(By.ID,"id_password").send_keys('adminTest')
+        self.driver.find_element(By.ID,"id-submitForm").click()
+
+
+    def tearDown(self):
+        self.driver.quit()
+        self.ong = None
+        self.test_stock_1 = None
+        super().tearDown()
+
+    def test_access_stock_view(self):
+        self.driver.get(f'{self.live_server_url}/stock/list')
+        self.assertTrue(self.driver.find_element(By.ID,"section-stock"))
