81dbbc72a64061227d964dde7f20b3f8a9724ef3
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-03-26 10:58:29 +0200

    FIX(#331): FIX CODACY AND COLOURS
    Home was green instead of blue

--- sponsorship/views.py
+++ sponsorship/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from functools import wraps
 from .models import Sponsorship
 from .forms import create_sponsorship_form
 from main.views import videssur_required
