53aebc68e3aa587a3f0f4b5df5f4eb230e15a50d
martirl11 <reyeslopezmarta2001@gmail.com>
2023-04-03 19:42:23 +0200

    FEAT(#480): SUBSIDY FILTER Created subsidy filter form and view

--- subsidy/views.py
+++ subsidy/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Subsidy
-from .forms import CreateNewSubsidy
+from .forms import CreateNewSubsidy, FilterSubsidyForm
 from datetime import date
 import json
 from decimal import Decimal
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from main.views import custom_403
+from django.db.models import Q
 
 
 
@@ -41,6 +42,12 @@ def subsidy_create(request):
 def subsidy_list(request):
     subsidies = Subsidy.objects.filter(ong=request.user.ong).values()
 
+    form = FilterSubsidyForm(request.GET or None)
+
+    if request.method == 'GET':
+        subsidies = subsidy_filter(subsidies, form)
+
+
     subsidies_dict = [obj for obj in subsidies]
     for s in subsidies_dict:
         s.pop('_state', None)
@@ -53,6 +60,7 @@ def subsidy_list(request):
         'object_name': 'subvención',
         'object_name_en': 'subsidy',
         'title': 'Gestión de Subvenciones',
+        'form': form,
     }
 
     return render(request, 'subsidy/list.html', context)
@@ -84,5 +92,80 @@ def subsidy_update(request, subsidy_id):
         return custom_403(request)
     return render(request, 'subsidy/create.html', {"form": form})
 
+def is_valid_queryparam(param):
+    return param != "" and param is not None
 
+def subsidy_filter(queryset, form):
+    
+    q = form['qsearch'].value()
+    min_presentation_date = form['min_presentation_date'].value()
+    max_presentation_date = form['max_presentation_date'].value()
+    min_payment_date = form['min_payment_date'].value()
+    max_payment_date = form['max_payment_date'].value()
+    min_provisional_resolution_date = form['min_provisional_resolution_date'].value()
+    max_provisional_resolution_date = form['max_provisional_resolution_date'].value()
+    min_final_resolution_date = form['min_final_resolution_date'].value()
+    max_final_resolution_date = form['max_final_resolution_date'].value()
+    organism = form['organism'].value()
+    name = form['name'].value()
+    ong = form['ong'].value()
+    status = form['status'].value()
+    amount_min = form['amount_min'].value()
+    amount_max = form['amount_max'].value()
+    
+
+    if q is not None:
+            if q.strip() != "":
+                queryset = queryset.filter(
+                    Q(organism__icontains=q) |
+                    Q(status__icontains=q) |
+                    Q(name__icontains=q) |
+                    Q(ong__name__icontains=q)
+                )
+
+    if is_valid_queryparam(min_presentation_date):
+        queryset = queryset.filter(presentation_date__gte=min_presentation_date)
+
+    if is_valid_queryparam(max_presentation_date):
+        queryset = queryset.filter(presentation_date__lte=max_presentation_date)
+
+    if is_valid_queryparam(min_payment_date):
+        queryset = queryset.filter(payment_date__gte=min_payment_date)
+
+    if is_valid_queryparam(max_payment_date):
+        queryset = queryset.filter(payment_date__lte=max_payment_date)
+    
+    if is_valid_queryparam(min_provisional_resolution_date):
+        queryset = queryset.filter(provisional_resolution__gte=min_provisional_resolution_date)
+
+    if is_valid_queryparam(max_provisional_resolution_date):
+        queryset = queryset.filter(provisional_resolution__lte=max_provisional_resolution_date)
+    
+    if is_valid_queryparam(min_final_resolution_date):
+        queryset = queryset.filter(final_resolution__gte=min_final_resolution_date)
+
+    if is_valid_queryparam(max_final_resolution_date):
+        queryset = queryset.filter(final_resolution__lte=max_final_resolution_date)
+
+    if is_valid_queryparam(organism):
+        queryset = queryset.filter(organism=organism)
+
+    if is_valid_queryparam(name):
+        queryset = queryset.filter(name=name)
+
+    if is_valid_queryparam(ong):
+        queryset = queryset.filter(ong__name=ong)
+
+    if is_valid_queryparam(status):
+        queryset = queryset.filter(status=status)
+
+    if is_valid_queryparam(amount_min):
+        queryset = queryset.filter(amount__gte=amount_min)
+
+    if is_valid_queryparam(amount_max):
+        queryset = queryset.filter(amount__lte=amount_max)
+
+    
+
+    return queryset
      
