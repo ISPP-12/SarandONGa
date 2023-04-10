007dd92137fcf69bff14b3a1594cdfe7d3ca7e4e
JaimeBorregoConde <jaimeborregoconde@gmail.com>
2023-04-10 15:43:30 +0200

    FEAT(#621): EXPORT ASEEM USER EXCEL
    New function to allow excel export, base on filtering

--- person/views.py
+++ person/views.py
@@ -12,6 +12,9 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 from django.db.models import Q
 
+from django.http import HttpResponse
+import csv
+
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -288,15 +291,31 @@ def child_list(request):
 def user_list(request):
     objects = ASEMUser.objects.filter(ong=request.user.ong).values()
 
+    form = FilterAsemUserForm(request.GET)
+    
+    objects = asemuser_filter(objects, FilterAsemUserForm(request.GET or None))
+
+    if request.method == 'POST':
+        try:           
+            response=HttpResponse()
+            response['Content-Disposition']= 'attachment; filename=asem_users.xlsx'
+            writer=csv.writer(response)
+            writer.writerow(['id','email','nombre','apellido','fecha_nacimiento','sexo','ciudad','direccion','telefono','codigo_postal','foto','tipo_usuario','es_miembro','condicion','tipo_correspondencia','estado','tamaño_unidad_familiar','casa_propia','vehiculo_propio','numero_cuenta_bancaria','ong'])
+            asemUser_fields=objects.values_list('id','email','name','surname','birth_date','sex','city','address','telephone','postal_code','photo','user_type','member','condition','correspondence','status','family_unit_size','own_home','own_vehicle','bank_account_number','ong')
+            for a in asemUser_fields:
+                writer.writerow(a)
+            message = ("Exportado correctamente")
+            messages.success(request, message)
+            return response
+        except:
+            message = ("Error in exporting data. There are null data in rows")
+            messages.error(request, message)
+            return render(request, 'users/list.html')
     paginator = Paginator(objects, 12)
     page_number = request.GET.get('page')
     user_page = paginator.get_page(page_number)
 
     title = "Gestión de Usuarios ASEM"
-    form = FilterAsemUserForm()
-    
-    if request.method == 'GET':
-        objects = asemuser_filter(objects, FilterAsemUserForm(request.GET))
 
     # depending of the user type write one title or another
     persons_dict = [user for user in user_page]
