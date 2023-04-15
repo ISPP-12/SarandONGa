5c66f4ac8d652a401a10c6e21c98d9255ccbb3e5
Jaime Moscoso Bernal <jaimesfc@live.com>
2023-04-15 14:14:53 +0200

    FIX(#682): FIX CODACY MINOR ISSUES
    Fix minor codacy issue

--- ong/signals.py
+++ ong/signals.py
@@ -8,7 +8,7 @@ def check_premium_plans(sender, user, request, **kwargs):
     # Buscamos todas las ONG con plan Premium y fecha de pago anterior a hace un mes
     one_month_ago = date.today() - timedelta(days=30)
     outdated_ongs = Ong.objects.filter(plan=PLAN_TYPES[1][0], 
-                                       premium_payment_date__lte=one_month_ago)
+        premium_payment_date__lte=one_month_ago)
 
     # Actualizamos el plan a Básico y eliminamos la fecha de pago premium
     for ong in outdated_ongs:
