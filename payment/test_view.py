d9b8fd44941835d99eebd03c29338058e30c559c
ezegonmac <ezegonmac01@gmail.com>
2023-05-06 14:06:36 +0200

    TEST(#730): CREATE EXPLORATORY TEST FOR THE ERROR

--- payment/test_view.py
+++ payment/test_view.py
@@ -1,5 +1,6 @@
 from ong.models import Ong
 from person.models import Worker
+from person.views import godfather_create
 from .models import Payment, Project, GodFather
 
 from datetime import datetime, timezone
@@ -11,9 +12,12 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PaymentViewTestCase(StaticLiveServerTestCase):
+    
     def setUp(self):
         super().setUp()
         self.usersuper = Worker(
@@ -37,6 +41,25 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
             ong=self.ong,
             paid=True,
         )
+        
+        self.godfather = GodFather.objects.create(
+            name='Johny',
+            surname='Deep',
+            dni='65004204T',
+            email='emailje@gmail.com',
+            birth_date=datetime(1990, 1, 24, tzinfo=timezone.utc),
+            payment_method='T',
+            bank_account_number='ES6621000418401234567198',
+            bank_account_holder='John Bruh',
+            bank_account_reference='1465 0100 72 2030876294',
+            amount=100,
+            frequency='M',
+            start_date=datetime(2023, 1, 24, tzinfo=timezone.utc),
+            termination_date=datetime(2024, 1, 24, tzinfo=timezone.utc),
+            notes='Some notes',
+            status='C', ong=self.ong
+        )
+        
         self.project = Project.objects.create(title="Título",
                                               country="Españita",
                                               start_date=datetime(
@@ -48,6 +71,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
                                               announcement_date=datetime(
                                                   2024, 1, 3),
                                               ong=self.ong)
+        
         self.project2 = Project.objects.create(title="Título2",
                                                      country="España",
                                                      start_date=datetime(
@@ -59,6 +83,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
                                                      announcement_date=datetime(
                                                          2024, 1, 3),
                                                      ong=self.ong)
+        
         self.god_test = GodFather.objects.create(
             name='John',
             surname='Doe',
@@ -76,6 +101,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
             notes='Some notes',
             status='S', ong=self.ong
         )
+        
         self.test_payment_1.save()
         self.usersuper.set_password("adminTest")
         self.usersuper.is_admin = True
@@ -92,6 +118,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         self.driver.find_element(By.ID, "id_password").send_keys("adminTest")
         self.driver.find_element(By.ID, "id-submitForm").click()
 
+
     def tearDown(self):
         self.driver.quit()
         self.ong = None
@@ -101,6 +128,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         self.project2 = None
         self.project2 = None
         super().tearDown()
+
 
     def test_payment_create_view_amount_error(self):
         before_count = Payment.objects.count()
@@ -122,6 +150,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         after_count = Payment.objects.count()
         self.assertTrue(before_count == after_count)
 
+
     def test_payment_create_view_concept_error(self):
         before_count = Payment.objects.count()
 
@@ -140,6 +169,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         # Check there is no new payment
         after_count = Payment.objects.count()
         self.assertTrue(before_count == after_count)
+
 
     def test_payment_create_view_negative_amount_error(self):
         before_count = Payment.objects.count()
@@ -161,6 +191,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         # Check there is no new payment
         after_count = Payment.objects.count()
         self.assertTrue(before_count == after_count)
+
 
     def test_payment_view_update(self):
         self.driver.get(
@@ -198,6 +229,7 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
         self.test_payment_1.refresh_from_db()
         self.assertTrue(self.test_payment_1.concept == new_concept)
 
+
     def test_payment_view_delete(self):
         self.driver.get(
             f"{self.live_server_url}/payment/" +
@@ -227,3 +259,39 @@ class PaymentViewTestCase(StaticLiveServerTestCase):
 
         after_count = Payment.objects.count()
         self.assertTrue(before_count - 1 == after_count)
+
+
+    def test_payment_view_exploratory_1(self):
+        # check fix/730: PAYMENT FORM NOT MANTAINING SUBJECT
+
+        # Acess form from list
+        self.driver.get(f"{self.live_server_url}/user/godfather/list")
+        self.driver.find_element(By.ID, f'card-list-item-{self.godfather.id}').click()
+        # await until add-payment a is loaded
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "add-payment")))
+        self.driver.find_element(By.ID, "add-payment").click()
+
+        # Fill and submit form
+        # await until the form is loaded
+        print(self.driver.current_url)
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "id_concept")))
+        self.driver.find_element(By.ID, "id_concept").send_keys("Pago Padrino 1")
+        self.driver.find_element(By.ID, "id_amount").send_keys("45")
+
+        # scroll to submit button and click
+        self.driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        # await until the button is loaded
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "submit")))
+        self.driver.find_element(By.ID, "submit").click()
+
+        # Check the url is correct
+        correct_url = f"{self.live_server_url}/payment/create?godfather={self.godfather.id}"
+        self.assertTrue(self.driver.current_url == correct_url)
+        
+        # Check godfather is selected
+        select = Select(self.driver.find_element(By.ID, 'id_godfather'))
+        self.assertTrue(self.godfather.name in select.first_selected_option.text)
